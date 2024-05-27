9d28c8d23d5ae6781dd2ada6b3becb5da49e242e
Trinh Ngoc Hoang Nam <nam140103@gmail.com>
2024-05-27 09:48:02 +0700

    commit

--- c02/begin/js/boolean-variable.js
+++ c02/begin/js/boolean-variable.js
@@ -1,10 +1,10 @@
 var inStock;
-var Shipping;
-instock = true;
-shiiping = false;
+var shipping;
+inStock = true;
+shipping = false;
 
-Var elStock = document.getElementById('Stock');
-elStock.className = instock	;
+var elStock = document.getElementById('stock');
+elStock.className = inStock	;
 
 var elShip = document.getElementById('shipping');
 elShip.className = shipping;
